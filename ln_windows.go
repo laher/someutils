11aeabcde4925226114296fe7a496f3c07e86984
Am Laher <am@laher.net.nz>
2013-11-24 21:38:01 +1300

    fixing ln_windows compilation error

--- ln_windows.go
+++ ln_windows.go
@@ -8,7 +8,7 @@ import (
 
 //TODO use exec and 'mklink.exe'
 func makeLink(target, linkName string, options LnOptions) error {
-	if *options.IsSymbolic {
+	if options.IsSymbolic {
 		return os.Symlink(target, linkName)
 	} else {
 		return os.Link(target, linkName)
