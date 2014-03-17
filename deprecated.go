494673026373ec224d3e713e4a1fce034f303849
Am Laher <am@laher.net.nz>
2014-03-17 21:50:52 +1300

    ported head, tail, cp to new structure

--- deprecated.go
+++ deprecated.go
@@ -4,10 +4,16 @@ import "errors"
 
 //Deprecated. See someutils/some.CatCli
 func Cat(call []string) error {
-	return errors.New("Use someutils/some.CatCli(...)")
+	return errors.New("Deprecated function. Please see package github.com/someutils/some")
 }
 
 //Deprecated. See someutils/some.BasenameCli
 func Basename(call []string) error {
-	return errors.New("Use someutils/some.BasenameCli(...)")
+	return errors.New("Deprecated function. Please see package github.com/someutils/some")
+}
+
+
+//Deprecated. See someutils/some.CpCli
+func Cp(call []string) error {
+	return errors.New("Deprecated function. Please see package github.com/someutils/some")
 }
