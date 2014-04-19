f06c421b55810efe9ec50a344562c54bef587c57
Am Laher <am@laher.net.nz>
2014-04-19 22:15:28 +1200

    fix for tr. Behaving much more similar to tr now.

--- some/tr.go
+++ some/tr.go
@@ -1,6 +1,7 @@
 package some
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/laher/someutils"
@@ -16,12 +17,11 @@ func init() {
 
 type SomeTr struct {
 	IsDelete     bool
-	IsComplement bool
-	IsReplace    bool
+	IsSqueeze    bool
+	isComplement bool // currently unused as I just don't get it.
 	set1         string
 	set2         string
-	inputs       []*regexp.Regexp
-	outputs      []string
+	translations map[*regexp.Regexp]string
 }
 
 func (tr *SomeTr) Name() string {
@@ -31,45 +31,120 @@ func (tr *SomeTr) ParseFlags(call []string, errWriter io.Writer) (error, int) {
 	flagSet := uggo.NewFlagSetDefault("tr", "[OPTION]... SET1 [SET2]", someutils.VERSION)
 	flagSet.SetOutput(errWriter)
 	flagSet.AliasedBoolVar(&tr.IsDelete, []string{"d", "delete"}, false, "Delete characters in SET1, do not translate")
+	flagSet.AliasedBoolVar(&tr.IsSqueeze, []string{"s", "squeeze-repeats"}, false, "replace each input sequence of a repeated character that is listed in SET1 with a single occurence of that character")
+	//Don't get the Complement thing. Just don't understand it right now.
+//	flagSet.AliasedBoolVar(&tr.isComplement, []string{"c", "complement"}, false, "use the complement of SET1")
 	err, code := flagSet.ParsePlus(call[1:])
 	if err != nil {
 		return err, code
 	}
 	sets := flagSet.Args()
 	if len(sets) > 0 {
-		err = tr.SetSet1(sets[0])
-		if err != nil {
-			return err, 1
-		}
+		tr.set1 = sets[0]
 	} else {
 		return errors.New("Not enough args supplied"), 1
 	}
 	if len(sets) > 1 {
-		err = tr.SetSet2(sets[1])
-		if err != nil {
-			return err, 1
-		}
-	} else if !tr.IsDelete && !tr.IsComplement {
+		tr.set2 = sets[1]
+	} else if !tr.IsDelete && !tr.isComplement {
 		return errors.New("Not enough args supplied"), 1
+	}
+	err = tr.Preprocess()
+	if err != nil {
+		return err, 1
 	}
 	return nil, 0
 }
-
+/*
 func (tr *SomeTr) SetSet1(set1 string) error {
 	tr.set1 = set1
-	//tr.inputs, err := convertSet1(tr.set1)
-	inputs, err := convertSet1(set1)
-	tr.inputs = inputs
-	return err
+	//inputs, err := convertSet1(set1)
+	//tr.inputs = inputs
+	//return err
 }
 func (tr *SomeTr) SetSet2(set2 string) error {
 	tr.set2 = set2
-	//tr.outputs, err := convertSet2(tr.set2)
-	outputs, err := convertSet2(set2)
-	tr.outputs = outputs
-	return err
+	//outputs, err := convertSet2(set2)
+	//tr.outputs = outputs
+	//return err
+}
+*/
+func (tr *SomeTr) Preprocess() error {
+	tr.translations = map[*regexp.Regexp]string{}
+	set1 := tr.set1
+
+	var set1Part, set2Part string
+	var set2len int
+	var err error
+	if tr.IsDelete {
+		// processSet1 only
+		for len(set1) > 0 {
+			set1Part, set1, _ = nextPartSet1(set1)
+			reg, err := tr.toRegexp(set1Part)
+			if err != nil {
+				return err
+			}
+			tr.translations[reg] = ""
+		}
+//	} else if tr.isComplement {
+		// dunno
+	} else {
+		// process both sets together
+		set2 := tr.set2
+		for len(set1) > 0 {
+			set1Part, set1, set2len = nextPartSet1(set1)
+			var set2New string
+			set2Part, set2New, err = nextPartSet2(set2, set2len)
+			if len(set2New) > 0 { //incase set2 is shorter than set1, behave like BSD tr (rather than SystemV, which truncates set1 instead)
+				set2 = set2New
+			}
+			if err != nil {
+				return err
+			}
+			reg, err := tr.toRegexp(set1Part)
+			if err != nil {
+				return err
+			}
+			tr.translations[reg] = set2Part
+		}
+	}
+	return nil
 }
 
+func (tr *SomeTr) toRegexp(set1Part string) (*regexp.Regexp, error) {
+	maybeSqueeze := ""
+	if tr.IsSqueeze {
+		maybeSqueeze = "+"
+	}
+	regString := "^["+set1Part+"]"+maybeSqueeze
+	//fmt.Println(regString)
+	reg, err := regexp.Compile(regString)
+	return reg, err
+}
+
+func nextPartSet1(set1 string) (string, string, int) {
+	if strings.HasPrefix(set1, "[") {
+		//find matching
+		if strings.Contains(set1, "]") {
+			return set1[:strings.Index(set1, "]")+1], set1[strings.Index(set1, "]")+1:], 1
+
+		} else {
+			return set1[:1], set1[1:], 1
+		}
+	} else if len(set1)>2 && set1[1]=='-' {
+		return set1[:3], set1[3:], 1
+	} else {
+		return set1[:1], set1[1:], 1
+	}
+}
+
+func nextPartSet2(set2 string, set2len int) (string, string, error) {
+	if len(set2) < set2len {
+		return "", "", errors.New(fmt.Sprintf("Error out of range (%d - %s)", set2len, set2))
+	}
+	return set2[:set2len], set2[set2len:], nil
+}
+/*
 //TODO fix behaviour of set1/set2 relationship
 func convertSet2(set2 string) ([]string, error) {
 	if strings.Contains(set2, "-") {
@@ -112,41 +187,43 @@ func convertSet1(set1 string) ([]*regexp.Regexp, error) {
 	}
 	return inputs, nil
 }
-
+*/
 func (tr *SomeTr) Invoke(invocation *someutils.Invocation) (error, int) {
 	invocation.ErrPipe.Drain()
 	invocation.AutoHandleSignals()
-	/*
-		inputs, err := convertSet1(tr.Set1)
-		if err != nil {
-			return err
-		}
-		outputs, err := convertSet2(tr.Set2)
-		if err != nil {
-			return err
-		}
-	*/
+	tr.Preprocess()
 	fu := func(inPipe io.Reader, outPipe2 io.Writer, errPipe io.Writer, line []byte) error {
 		//println("tr processing line")
-		out := string(line)
-		for i, reg := range tr.inputs {
-			var output string
-			if len(tr.outputs) > i {
-				output = tr.outputs[i]
-			} else {
-				output = tr.outputs[len(tr.outputs)-1]
+		//outline := line
+		var buffer bytes.Buffer
+		remainder := string(line)
+		for len(remainder) > 0 {
+			nextPart := remainder[:1]
+			for reg, v := range tr.translations {
+				//fmt.Printf("Translation '%v'=>'%s' on '%s'\n", reg, v, remainder)
+		
+				if reg.MatchString(remainder) {
+					toReplace := reg.FindString(remainder)
+					replacement := reg.ReplaceAllString(toReplace, v)
+				//	fmt.Printf("Match, %s=>%s\n", toReplace, replacement)
+					nextPart = replacement
+					if len(toReplace) > 1 { //if squeezing has taken place, remove more leading chars accordingly
+				//		fmt.Printf("Squeezing! %d \n", len(toReplace))
+						remainder = remainder[len(toReplace)-1:]
+					}
+					break
+				} else {
+
+	//				fmt.Printf("No match\n")
+				}
 			}
-			out = reg.ReplaceAllString(out, output)
+			remainder = remainder[1:]
+			buffer.WriteString(nextPart)
 		}
-		//	fmt.Printf("From %v to %v\n", string(line), string(out))
-		//println("tr printing line: |", out, "|")
+		out := buffer.String()
 		_, err := fmt.Fprintln(outPipe2, out)
-		//println("tr processed line")
 		return err
 	}
-	//fmt.Printf("from %v\n", tr.inputs)
-	//fmt.Printf("to %v\n", tr.outputs)
-
 	err := someutils.LineProcessor(invocation.MainPipe.In, invocation.MainPipe.Out, invocation.ErrPipe.Out, fu)
 	if err != nil {
 		return err, 1
@@ -159,20 +236,20 @@ func NewTr() *SomeTr {
 }
 func Tr(set1, set2 string) someutils.CliPipable {
 	tr := NewTr()
-	tr.SetSet1(set1)
-	tr.SetSet2(set2)
+	tr.set1 = set1
+	tr.set2 = set2
 	return (tr)
 }
 func TrD(set1 string) someutils.CliPipable {
 	tr := NewTr()
 	tr.IsDelete = true
-	tr.SetSet1(set1)
+	tr.set1 = set1
 	return (tr)
 }
 func TrC(set1 string) someutils.CliPipable {
 	tr := NewTr()
-	tr.IsComplement = true
-	tr.SetSet1(set1)
+	tr.isComplement = true
+	tr.set1 = set1
 	return (tr)
 }
 
